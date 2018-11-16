c5700b69505846513c64fe538ca5658fc4b81fef
Brett Stainken <brettstainken.student@careerdevs.com>
2018-11-16 00:09:27 +0000

    First Commit

---
+++ BoolWNL.js
@@ -0,0 +1,12 @@
+var todolist = {
+    todos:[],
+    displayTodos:function() {
+        console.log('My Todos', this.todos);
+    },
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+    }
+}
